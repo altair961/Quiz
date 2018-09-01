6f9bd9f441630a4d637d36b1e17964b4bbd01517
altair961 <altair961@users.noreply.github.com>
2018-09-01 16:43:50 +0200

    Added question component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, QuestionComponent
   ],
   imports: [
     BrowserModule
