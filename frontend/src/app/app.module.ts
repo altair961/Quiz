b743b04a8c8fdf273d3cc0a0e082061d62023554
altair961 <altair961@users.noreply.github.com>
2018-09-01 17:41:43 +0200

    Removed default template file and its CSS file, added Card element

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
@@ -11,7 +11,11 @@ import { QuestionComponent } from './question.component';
     AppComponent, QuestionComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MatButtonModule, MatInputModule
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
