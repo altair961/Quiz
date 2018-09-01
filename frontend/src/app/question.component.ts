46a80f633609c82d7238f18dd558b41589c11e71
altair961 <altair961@users.noreply.github.com>
2018-09-01 17:19:23 +0200

    Added a button to the template

--- frontend/src/app/question.component.ts
+++ frontend/src/app/question.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'question',
-    template: 'this is our question'
+    template: 'this is our question <button mat-button>test</button>'
 })
 export class QuestionComponent {
 
